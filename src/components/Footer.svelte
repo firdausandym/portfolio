1fcccacb83177a221e9e69f16f6565f5ecc1cdb5
Firdaus Andy Muhammad <firdausandym@gmail.com>
2024-03-03 22:11:50 +0700

    porto v1

---
+++ src/components/Footer.svelte
@@ -0,0 +1,61 @@
+<footer
+  class="main shadow-2xl py-32 z-50 bg-gray-50 dark:bg-gray-700 bg-gradient-to-b dark:from-gray-800 dark:to-gray-900"
+>
+  <ul data-aos="fade-down" class="flex item-center justify-center gap-6 mb-8">
+    <li class="m-0">
+      <a
+        title="View my GitHub profile"
+        aria-label="See my GitHub profile"
+        href="https://github.com/firdausandym"
+        target="_blank"
+        class="nav-text p-1"><i class="fab fa-github fa-lg" /></a
+      >
+    </li>
+    <li class="m-0">
+      <a
+        title="Connect on LinkedIn"
+        aria-label="Connect on LinkedIn"
+        href="https://www.linkedin.com/in/firdausandym/"
+        target="_blank"
+        class="nav-text p-1"
+      >
+        <i class="fab fa-linkedin-in fa-lg" /></a
+      >
+    </li>
+    <li class="m-0">
+      <a
+        title="Send me an email"
+        aria-label="Send me an email"
+        href="mailto:firdausandym@gmail.com"
+        target="_blank"
+        class="nav-text p-1"
+      >
+        <i class="far fa-envelope fa-lg" />
+      </a>
+    </li>
+
+    <!-- <a href="https://www.instagram.com/n.bob.s/" target="_blank"
+        ><i class="text-gray-800 dark:text-gray-50 fab fa-instagram fa-lg" /></a
+      > -->
+    <li class="m-0">
+      <a
+        title="View my Codepens"
+        aria-label="View my Codepens"
+        href="https://codepen.io/firdausandym"
+        target="_blank"
+        class="nav-text p-1"><i class="fab fa-codepen fa-lg" /></a
+      >
+    </li>
+  </ul>
+
+  <div class="text-center">
+    <a
+      data-aos="fade-up"
+      class="nav-text text-gray-700 dark:text-gray-50"
+      href="https://github.com/firdausandym/Portfolio-V2"
+      target="_blank"
+    >
+      <!-- Designed & Built by Ng Bob Shoaun &copy; <time>2023</time> -->
+    </a>
+  </div>
+</footer>
---
+++ src/resume.js
@@ -0,0 +1,220 @@
+const resume = {
+  name: "Firdaus Andy Muhammad",
+  title: "Software Developer",
+  phoneNumber: "082233905206",
+  website: {
+    url: "https://firdausandym.github.io/",
+    label: "andy-dev",
+  },
+  email: {
+    url: "firdausandym@gmail.com",
+    label: "firdausandym@gmail",
+  },
+  linkedIn: {
+    url: "https://id.linkedin.com/in/firdaus-andy-muhammad-82071b219",
+    label: "linkedin.com/in/firdausandym",
+  },
+  github: {
+    url: "https://github.com/firdausandym",
+    label: "github.com/firdausandym",
+  },
+  location: "Pasuruan, East Java, Indonesia",
+  about:
+    // "Skilled and diligent programmer with an eye for good designs. Always seeking to learn and improve on existing ways. A resourceful and avid self-learner passionate in coding.",
+    "Accomplished and diligent software developer with a good eye for designs. Able to deliver products with high quality proven through customer satisfaction. Friendly, with strong communication skills with teammates. Seeking to learn and grow to become a professional software engineer.",
+
+  skills: {
+    languages : [
+      "JavaScript (ES6)",
+      "TypeScript",
+      "C#",
+      "c++",
+    ],
+    frontend: [
+      "Angular",
+      "Ionic",
+      ".NET",
+      "Vue",
+      "Svelte",
+      "Flutter",
+    ],
+    backend: [
+      "Express JS",
+      "Nest JS",
+      ".NET",
+    ],
+    database: ["MongoDB", "SQL", "SQL Server"],
+    devops: [
+      "AWS",
+      "Firebase",
+      "Google Cloud",
+      "Heroku",
+      "Docker",
+      "Netlify",
+      "Github Actions",
+      "Splunk",
+      "Jenkins",
+    ],
+    others: [
+      "Linux",
+      "Git",
+      "NPM",
+      "Insomnia",
+      "Postman",
+      "Fiddler",
+      "GraphQL",
+      "Arduino",
+    ],
+  },
+
+  // Ukulele
+  interests: ["Drums", "Graphic Design", "PC Building", "Table Tennis"],
+  languages: ["English", "Chinese", "Malay"],
+  lastUpdated: "Dec 2023",
+  education: {
+    school: "State Polytechnic of Jember",
+    cgpa: "3.61 / 4.0", // Dean's List Scholar
+    description:
+      "Associate's degree - Computer Engineering",
+    timeline: "2021",
+  },
+
+  experiences: [
+    {
+      company: "Tirta Dharma PDAM Kota Pasuruan",
+      position: "Intern - IT Staff",
+      timeline: "Sep 2020 - Des 2020",
+      location: "Pasuruan, East Java, Indonesia",
+      points: [
+        "Contributing in the Technical Department to procure, troubleshoot, and maintain the hardware as well as software required by the employees at the Pasuruan City Water Utility.",
+        "Creating a Water Turbidity Monitoring Device with Turbidity Sensor and Solar Cells Based on Arduino Nano."
+      ],
+    },
+    {
+      company: "Elka Code",
+      position: "Backend Developer",
+      timeline: "Nov 2021 - April 2023",
+      location: "Pasuruan, East Java, Indonesia",
+      points: [
+        "Participating in the backend development and crafting various APIs.",
+        "within a project called Bricks App located in its home country, France, focusing on Architecture and Construction with numerous exciting features."
+      ],
+    },
+    {
+      company: "PT Cebes Indonesia",
+      position: "Software Developer",
+      timeline: "Mei 2023 - Now",
+      location: "Surabaya, East Java, Indonesia",
+      points: [        
+        "PT CEBES INDONESIA is a Dutch company with over 30 years of experience in software development, serving numerous clients. The Indonesian branch was established in 2006. PT CEBES INDONESIA specializes in developing high-quality web-based applications. Since then, several significant applications have been developed for the European market.",
+        "Project: Viper, Darmawisata Indonesia",
+        "Description: to be continued",
+      ],
+    },
+    // {
+    //   company: "Pathforge Pte. Ltd.",
+    //   position: "Full-Stack Developer",
+    //   timeline: "Sept 2020 - April 2021",
+    //   location: "Kuala Lumpur, Malaysia",
+    //   points: [
+    //     "Developed a social e-learning site using Vue and Bulma, with GraphQL, Hasura, and Postgres in the backend.",
+    //     "Built a CMS for course materials, integrated user leaderboards, and implemented an entire social feed and endorsement system.",
+    //     "Conducted Zoom lectures about programming and web development, teaching HTML, CSS, and JavaScript to groups of learners.",
+    //   ],
+    // },
+    // {
+    //   company: "N2N Connect Bhd.",
+    //   position: "Industrial Trainee",
+    //   timeline: "May 2019 - Aug 2019",
+    //   location: "Kuala Lumpur, Malaysia",
+    //   points: [
+    //     "Revamped timesheet management system written in Angular.js and Bootstrap by introducing QoL features and fixing bugs.",
+    //     // "Presented and showcased work during head of department meetings, collecting critical feedback and suggestions.",
+    //     "Authored extensive technical documentation for a proper project handover, saving 70% of on-boarding time for new developers.",
+    //   ],
+    // },
+  ],
+  projects: [
+    {
+      name: "Bricks App",
+      type: "Industrial Project",
+      points: [
+        // "A proof of work cryptocurrency built using Typescript Node.js for business logic, Express, MongoDB, and web sockets for the Node backend/API, Jest for unit testing, React, Redux, and Bulma for the frontend block explorer, wallet software, and mining client.",
+        // "Secured using cryptographic hash algorithms, digital signatures, public-key cryptography, Merkle trees, Nakamoto consensus, BIP compliance, and the UTXO model, resulting in a tamper-proof blockchain ledger.",
+        // // "Refactored blockchain storage, query, and validation code by redefining Mongoose schemas, and devising a new stratagy for handling blockchain reorganizations and forks. Overall, increasing efficiency and scalability by 80%.",
+        // "Through this project, taught 20+ friends and family basic cryptocurrency usage, and grew a Discord community with 100+ members.",
+        "A collaborative and flexible application for architecture and construction projects.",
+        "Facilitating team management and ensuring timely project completion.",
+        "Featuring compatible BIM platform integrations.",
+        "Programming languages used include JavaScript and TypeScript.",
+        "Utilizing the Node.js runtime.",
+        "Employing the Express.js and Nest.js frameworks.",
+        "Developing Web APIs using GraphQL and REST API.",
+        "Managed with a NoSQL database (MongoDB)."
+      ],
+      link: "https://github.com/firdausandym/Bobcoin-Node",
+      github: [
+        {
+          repo: "Bobcoin-Node",
+          url: "https://github.com/firdausandym/Bobcoin-Node",
+        },
+        {
+          repo: "Bobcoin-Client",
+          url: "https://github.com/firdausandym/Bobcoin-Client",
+        },
+      ],
+    },
+    {
+      name: "Darmawisata Indonesia",
+      type: "Tour & Travel Project",
+      points: [
+        "Developed an art hosting web app that allows artists to create a job-ready portfolio to showcase their artworks.",
+        "Led and managed a team of 4 junior developers, providing mentorship and support to struggling individuals.",
+        "Built using React, Tailwind, Redux for the frontend. Express, MongoDB, and Cloudinary for the backend. Heroku, Google Cloud for deployment. Jest and Supertest for testing. OAuth2.0 for authentication using Google or Facebook.",
+        // "Wrote proper documentation for developer on-boarding, for every user story, and for every API endpoint.",
+      ],
+      link: "https://github.com/firdausandym/Artsu.me",
+      github: [
+        {
+          repo: "Artsu.me",
+          url: "https://github.com/firdausandym/Artsu.me",
+        },
+      ],
+    },
+    // {
+    //   name: "Assembly Doodle Jump",
+    //   type: "Academic project",
+    //   points: [
+    //     "Final coursework using MIPS assembly language, scored a grade of 100%.",
+    //     "Features sound effects, powerups like springs and jetpacks, different platform types, and scalable difficulty.",
+    //   ],
+    // },
+    // {
+    //   name: "Course Checker",
+    //   type: "Personal project",
+    //   points: [
+    //     "Aids in course planning for upcoming semester by dynamically generating a visual timetable given a selection of courses.",
+    //     "Highly customizable in terms of colors, additional info, and timestamps",
+    //     "Developed with C# using the Unity Engine, built as a WebGL frame embedded into a website made with HTML + CSS.",
+    //   ],
+    // },
+    // {
+    //   name: "Water Chef",
+    //   type: "Personal project",
+    //   points: [
+    //     "A fast paced restaurant simulator game developed with Unity Engine and written in C#.",
+    //     "Complete with multiple levels, objectives, animations, music, and save load functionality.",
+    //   ],
+    // },
+    // {
+    //   name: "Fishackathon 2018",
+    //   type: "by HackerNest",
+    //   points: [
+    //     "Built an app to generate awareness of region specific fishing laws.",
+    //     "Presented to judges working in the fishing industry.",
+    //   ],
+    // },
+  ],
+};
+
+export default resume;
